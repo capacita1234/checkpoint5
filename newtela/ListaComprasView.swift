42d8b5bf1715c8d3be6a86473178ac345dab67f4
equipe24ios <equipe24.ioscapacitabr@gmail.com>
2025-08-12 13:30:40 -0300

    checkpoint 5

--- newtela/ListaComprasView.swift
+++ newtela/ListaComprasView.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct ListaComprasView: View {
+    @State private var listas: [Lista] = [
+        Lista(titulo: "Ferramentas", descricao: "Itens para conserto", cor: .red, icone: "hammer"),
+        Lista(titulo: "Frutas", descricao: "Frutas do mercado", cor: .yellow, icone: "pencil"),
+        Lista(titulo: "Mercado", descricao: "Compras semanais", cor: .green, icone: "cart")
+    ]
+    
     var body: some View {
-        NavigationStack{
+        NavigationStack {
             VStack(spacing: 16) {
-                
                 
                 Text("Listas de Compras")
                     .font(.title2)
@@ -33,36 +38,29 @@ struct ListaComprasView: View {
                 
                 ScrollView {
                     VStack(spacing: 12) {
-                        ListaItemView(cor: .red, icone: "hammer", titulo: "Título", descricao: "Descrição")
-                        ListaItemView(cor: .yellow, icone: "creditcard", titulo: "Título", descricao: "Descrição")
-                        ListaItemView(cor: .orange, icone: "barcode", titulo: "Título", descricao: "Descrição")
-                        ListaItemView(cor: .green, icone: "cart", titulo: "Título", descricao: "Descrição")
-                        ListaItemView(cor: .pink, icone: "house", titulo: "Título", descricao: "Descrição")
+                        ForEach(listas) { lista in
+                            ListaItemView(cor: lista.cor, icone: lista.icone, titulo: lista.titulo, descricao: lista.descricao)
+                        }
                     }
                     .padding(.horizontal)
                     .padding(.top, 5)
                 }
                 
-                NavigationLink{
-                    CriarLista()
-                } label:{
-                    HStack{
-                        Text("Criar Lista")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.black)
-                            .cornerRadius(12)
-                    }
-                    .padding(.horizontal)
-                    .padding(.bottom, 5)
-                    
+                
+                NavigationLink {
+                    CriarLista(listas: $listas)
+                } label: {
+                    Text("Criar Lista")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.black)
+                        .cornerRadius(12)
                 }
-              
-                
-                
+                .padding(.horizontal)
+                .padding(.bottom, 5)
             }
-        } }
+        }
+    }
 }
-
